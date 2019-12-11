407706485cc81d8ffb2c0b6477b704a1a43e5cb5
esilvac <esilvac59@gmail.com>
2019-12-11 17:41:29 -0500

    Adding atlas database url

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const token = localStorage.getItem('authenticationToken');
 const Api = axios.create({
-  baseURL: "http://localhost:3001/",
+  baseURL: "https://obscure-lowlands-95572.herokuapp.com/",
   timeout: 5000,
   headers: {
     "Access-Control-Allow-Origin": "*",
